f8fb42b3003f8615782f1be3c87dcd10385261a7
Teo Calvo <teocalvo2@gmail.com>
2025-03-27 01:00:49 -0300

    feat: pos aula missing

---
+++ pandas_base/dia03/05_prim_transacao_cli.py
@@ -0,0 +1,24 @@
+# %%
+
+import pandas as pd
+
+df = pd.read_csv("../data/transacoes.csv")
+df.head()
+
+# %%
+
+(df.sort_values(["dtCriacao"])
+   .drop_duplicates(subset=["idCliente"], keep='first'))
+
+# %%
+
+(df.sort_values(["dtCriacao"])
+   .drop_duplicates(subset=["idCliente"], keep='first'))
+
+# %%
+
+# 05.05 - Selecione a primeira transação diária de cada cliente.
+
+df["diaCriacao"] = pd.to_datetime(df["dtCriacao"]).dt.date
+(df.sort_values("dtCriacao")
+   .drop_duplicates(subset=["idCliente", "diaCriacao"]))
